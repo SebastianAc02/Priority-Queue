54511aa037585842670b8d455085f9e88e3f0f1a
Sebastian Acosta <sacostamolina@outlook.com>
2022-10-21 15:30:37 -0700

    Fixed linking error, finished peek method

---
+++ main.cpp
@@ -0,0 +1,31 @@
+//
+//  Main.cpp
+//  LAB02-CMPT
+//
+//  Created by Sebastian Acosta Molina on 2022-10-21.
+//
+
+
+
+#include "PriorityQ.hpp"
+
+
+int main()
+{
+   
+    PriorityQ<char> newQueu;
+    
+  
+    newQueu.enqueue('c', 2);
+    
+     char x = newQueu.peek();
+    
+    std::cout << x;
+   
+    newQueu.enqueue('p', 1);
+    
+    x = newQueu.peek();
+    
+    std::cout << x;
+    
+}
