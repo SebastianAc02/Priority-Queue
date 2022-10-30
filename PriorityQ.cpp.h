d2ac00d81d7d7b6ff497f2216989340273738e9f
anursaidy <ansnursaidy@gmail.com>
2022-10-30 13:25:33 -0700

    Move semantics

--- PriorityQ.cpp.h
+++ PriorityQ.cpp.h
@@ -1,6 +1,5 @@
 #include "PriorityQ.hpp"
 
-
 template<typename T>
 PriorityQ<T>::PriorityQ()
 {
@@ -11,19 +10,33 @@ PriorityQ<T>::PriorityQ()
 	}
 }
 
+template<typename T>
+PriorityQ<T>::PriorityQ(const PriorityQ<T>& anotherQ)
+{
+	for (int i = 0; i < MAX_NUM_OF_LEVEL; i++)
+	{
+		head_MultilevelQ[i] = copy(anotherQ.head_MultilevelQ[i], anotherQ.tail_MultilevelQ[i]);
+	}
+}
+
+
+template <typename T>
+PriorityQ<T>::PriorityQ(PriorityQ<T>&& anotherQ)
+{
+	for (int i = 0; i < MAX_NUM_OF_LEVEL; i++)
+	{
+		head_MultilevelQ[i] = anotherQ.head_MultilevelQ[i];
+		tail_MultilevelQ[i] = anotherQ.tail_MultilevelQ[i];
+	}
+}
+
 
 template<typename T>
 PriorityQ<T>::~PriorityQ()
 {
-
-	for (int i = 0; i < 10; i++)
-	{
-		delete head_MultilevelQ[i];
-		delete tail_MultilevelQ[i];
-
-	}
-
+	clear();
 }
+
 
 template<typename T>
 void PriorityQ<T>::enqueue(const T& element, const int lv)
@@ -37,9 +50,8 @@ void PriorityQ<T>::enqueue(const T& element, const int lv)
 		tail_MultilevelQ[lv - 1]->next = new Node<T>(element);
 		tail_MultilevelQ[lv - 1] = tail_MultilevelQ[lv - 1]->next;
 	}
-
-
 }
+
 template <typename T>
 T PriorityQ<T>::dequeue() {
 	if (is_MultilevelQ_empty()) {
@@ -47,7 +59,7 @@ T PriorityQ<T>::dequeue() {
 	}
 
 	//check from the highest priority lv
-	for (unsigned int i = 9; ; i--) //condition of for-loop is left intentionally
+	for (unsigned int i = MAX_NUM_OF_LEVEL - 1; ; i--) //condition of for-loop is left intentionally
 	{
 		if (head_MultilevelQ[i] != nullptr) // if(MultilevelQ is NOT empty - as checked above). This if condition must be met
 		{
@@ -85,13 +97,11 @@ bool PriorityQ<T>::is_levelQ_empty(int lv) {
 }
 
 
-
 template <typename T>
-T PriorityQ<T>::peek() {
+T PriorityQ<T>::peek() const {
 
-	if (is_MultilevelQ_empty()) {
+	if (is_MultilevelQ_empty())
 		throw runtime_error("Queue is empty");
-	}
 
 	//check from the highest priority lv
 	for (unsigned int i = 9; ; i--) //condition of for-loop is left intentionally
@@ -101,7 +111,73 @@ T PriorityQ<T>::peek() {
 			return head_MultilevelQ[i]->element;
 		}
 	}
+}
 
+
+template <typename T>
+PriorityQ<T>& PriorityQ<T>::operator= (const PriorityQ<T>& anotherQ)
+{
+	clear();
+	if (this != &anotherQ)
+	{
+		for (unsigned int i = 0; i < MAX_NUM_OF_LEVEL; i++)
+			head_MultilevelQ[i] = copy(anotherQ.head_MultilevelQ[i], anotherQ.tail_MultilevelQ[i]);
+
+	}
+	return *this;
+}
+
+template <typename T>
+PriorityQ<T>& PriorityQ<T>::operator= (const PriorityQ<T>&& anotherQ)
+{
+	clear();
+	for (unsigned int i = 0; i < MAX_NUM_OF_LEVEL; i++)
+	{
+		head_MultilevelQ[i] = anotherQ.head_MultilevelQ[i];
+		tail_MultilevelQ[i] = anotherQ.tail_MultilevelQ[i];
+
+		anotherQ.head_MultilevelQ[i] = anotherQ.tail_Multilevel[i] = nullptr;
+	}
+	return *this;
 
 }
 
+template <typename T>
+static Node<T> copy(Node<T>* head, Node<T>*& tail)
+{
+	// if (queue is empty)
+	if (head == nullptr)
+	{
+		tail = nullptr;
+		return nullptr;
+	}
+
+	Node<T>* temp_newNode = new Node<T>(head->element);
+
+	// if (queue has exactly one element)
+	if (head->next == nullptr)
+	{
+		tail = temp_newNode;
+		return temp_newNode;
+	}
+
+	temp_newNode->next = copy(head->next, tail);
+	return temp_newNode;
+}
+
+template <typename T>
+void PriorityQ<T>::clear()
+{
+	for (int i = 0; i < MAX_NUM_OF_LEVEL; i++)
+	{
+		Node<T>* temp_head = head_MultilevelQ[i];
+		while (head_MultilevelQ[i] != nullptr)
+		{
+			Node<T>* temp = temp_head->next;
+			delete temp_head;
+			temp_head = temp;
+
+			tail_MultilevelQ[i] = nullptr;
+		}
+	}
+}
--- PriorityQ.hpp
+++ PriorityQ.hpp
@@ -2,6 +2,7 @@
 #define PRIORITY_Q_hpp
 #include <iostream>
 
+const int MAX_NUM_OF_LEVEL = 10;
 using namespace std;
 
 template <typename T>
@@ -20,14 +21,16 @@ public:
 		this->element = element;
 		next = nullptr;
 	}
-	  ~Node(){
-       if(next != nullptr)
-       {
-           delete next;
-           next = nullptr;
-       }
-        
-    }
+
+	~Node()
+	{
+		if (next != nullptr)
+		{
+			delete next;
+			next = nullptr;
+		}
+	}
+
 };
 
 
@@ -37,25 +40,35 @@ class PriorityQ
 {
 public:
 	PriorityQ();
-	~PriorityQ();
+	virtual ~PriorityQ();
+	PriorityQ(const PriorityQ<T>& anotherQ);
+	PriorityQ(PriorityQ<T>&& anotherQ);
+
 
 	void enqueue(const T& item, const int lv);
 	T dequeue();
-	T peek();
+	T peek() const;
+
+	PriorityQ<T>& operator= (const PriorityQ<T>& anotherQ);
+	PriorityQ<T>& operator= (const PriorityQ<T>&& anotherQ);
+
+	static Node<T> copy(Node<T>* head, Node<T>*& tail);
+	void clear();
+
+
+	/* to_be_implemented
+	* operator= & move operator=
+	* Copy & Move constructors
+	*/
+
 private:
-	
+
 	bool is_MultilevelQ_empty();
 	bool is_levelQ_empty(int lv);
 
-	
-	Node<T>* head_MultilevelQ[10];
-	Node<T>* tail_MultilevelQ[10];
-
-
+	Node<T>* head_MultilevelQ[MAX_NUM_OF_LEVEL];
+	Node<T>* tail_MultilevelQ[MAX_NUM_OF_LEVEL];
 };
-
-
-
 
 #include "PriorityQ.cpp.h"
 #endif
